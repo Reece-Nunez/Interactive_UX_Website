fdf58b0ea4399d16e176b55d3063356b73a9007f
Reece Nunez <reecenunez@hotmail.com>
2023-10-14 06:18:27 -0500

    added get to taniti to navbar

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -42,3 +42,18 @@ function shiftRight() {
     }, 500);
 
 }
+
+// Get the navbar element
+const navbar = document.querySelector('.nav-bar');
+
+// Function to toggle the "scrolled" class based on scroll position
+function toggleNavbar() {
+    if (window.scrollY > 100) { // Adjust the scroll position threshold as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+}
+
+// Add the scroll event listener
+window.addEventListener('scroll', toggleNavbar);
