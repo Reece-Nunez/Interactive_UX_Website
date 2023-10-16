3ad296868876549f99958fabfd17a2dec4a4d18a
Reece <reecenunez@hotmail.com>
2023-10-16 04:19:00 -0500

    finished accom page

--- accom-page/accom.js
+++ accom-page/accom.js
@@ -37,3 +37,56 @@ sortSelect.addEventListener('change', function () {
         sortableList.appendChild(item);
     });
 });
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  let hasStarted = false; // Variable to ensure the animation runs only once
+  
+  window.addEventListener('scroll', function() {
+    const container = document.querySelector('.image-title');
+  
+    if (isInViewport(container) && !hasStarted) {
+      hasStarted = true; // Set to true so the animation doesn't repeat
+  
+      let findText = "Find The";
+      let perfectText = " Perfect Room";
+  
+      let textContainer = document.querySelector('.image-title');
+  
+      textContainer.innerHTML = ""; // Empty the container first
+  
+      let index1 = 0;
+      let index2 = 0;
+  
+      function typeFind() {
+        if (index1 < findText.length) {
+          textContainer.innerHTML += findText[index1];
+          index1++;
+          setTimeout(typeFind, 400); // 400 milliseconds between each letter
+        } else {
+          setTimeout(typePerfect, 1000); // Wait a second before starting the next text
+        }
+      }
+  
+      function typePerfect() {
+        if (index2 < perfectText.length) {
+          textContainer.innerHTML += perfectText[index2];
+          index2++;
+          setTimeout(typePerfect, 200); // 200 milliseconds between each letter
+        }
+      }
+  
+      textContainer.style.visibility = "visible";
+      typeFind(); // Start typing "Find The"
+    }
+  });
+  
